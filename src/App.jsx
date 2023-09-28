5e3dd9dc3dfd8c83ac017707e1baa3d348f8f478
Ahmed Gamal <agamal0077@gmail.com>
2023-09-28 18:26:13 +0300

    Navigation Component + Background Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navigation from './Components/Navigation/Navigation'
 
 export default function App() {
   return (
     <div className="App">
       <Navigation />
-      <Logo />
+      {/* {<Logo />
       <ImageLinkForm />
-      <FaceRecognition />
+      <FaceRecognition />} */}
     </div>
   )
 }
---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className="flex justify-end">
+      <button className=" p-6  font-bold  underline">
+        Sign Out
+      </button>
+    </nav>
+  )
+}
+
+export default Navigation
