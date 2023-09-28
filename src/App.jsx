06d955b5603257ca82e499c8610cae0b9c5aa554
Ahmed Gamal <agamal0077@gmail.com>
2023-09-28 16:13:14 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
+    </div>
+  )
+}
