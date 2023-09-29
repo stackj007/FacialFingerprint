243552ceb8540e62a8a9c986e71bd5537225744e
Ahmed Gamal <agamal0077@gmail.com>
2023-09-29 16:46:43 +0300

    create form component + styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 
 export default function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-
-      {/* {
       <ImageLinkForm />
+
+      {/*
       <FaceRecognition />} */}
     </div>
   )
---
+++ src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -0,0 +1,24 @@
+import './ImageLinkForm.css'
+
+const ImageLinkForm = () => {
+  return (
+    <div className="p-5 w-[700px] mx-auto">
+      <p className="text-center font-bold text-lg m-5">
+        This will detect faces in your pictures, give it a
+        try
+      </p>
+
+      <div className="Form flex justify-center shadow-md p-4">
+        <input
+          className="w-[55%]  border-2 border-gray-300 rounded-md"
+          type="text"
+        />
+        <button className="w-[15%] ml-2  bg-blue-500 text-white font-bold rounded-md">
+          Detect
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ImageLinkForm
