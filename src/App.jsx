e66520b148f649d76e68ba254b9bd4e558a81d7c
Ahmed Gamal <agamal0077@gmail.com>
2023-10-05 14:40:52 +0300

    adding input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,72 @@
-import { useState } from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import Rank from './Components/Rank/Rank'
-
-import { useCallback } from 'react'
 import { Particles } from 'react-tsparticles'
-
 import { loadSlim } from 'tsparticles-slim'
 import { loadLinksPreset } from 'tsparticles-preset-links'
 
-export default function App() {
-  const particlesInit = useCallback(async (engine) => {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  // #######
+  // particles
+  particlesInit = async (engine) => {
     console.log(engine)
     await loadSlim(engine)
     await loadLinksPreset(engine)
-  }, [])
-
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container)
-  }, [])
-
-  const particlesOptions = {
-    preset: 'links',
-    background: {
-      color: {
-        value: '',
-      },
-    },
-    particles: {
-      links: {
-        color: '#3ca9d1',
-        opacity: 5,
-      },
-    },
   }
 
-  return (
-    <div className="App">
-      <Particles
-        className="particles"
-        id="tsparticles"
-        options={particlesOptions}
-        init={particlesInit}
-        loaded={particlesLoaded}
-      />
+  particlesLoaded = async (container) => {
+    await console.log(container)
+  }
 
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />} */}
-    </div>
-  )
+  render() {
+    const particlesOptions = {
+      preset: 'links',
+      background: {
+        color: {
+          value: '',
+        },
+      },
+      particles: {
+        links: {
+          color: '#3ca9d1',
+          opacity: 5,
+        },
+      },
+    }
+    // #######
+    // particles
+
+    return (
+      <div className="App">
+        <Particles
+          className="particles"
+          id="tsparticles"
+          options={particlesOptions}
+          init={this.particlesInit}
+          loaded={this.particlesLoaded}
+        />
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} />
+      </div>
+    )
+  }
 }
+
+export default App
--- src/Components/ImageLinkForm/ImageLinkForm.jsx
+++ src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,6 +1,6 @@
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange }) => {
   return (
     <div className="p-5 w-full mx-auto flex flex-col items-center">
       <p className="text-center font-bold text-lg m-5">
@@ -12,6 +12,7 @@ const ImageLinkForm = () => {
         <input
           className="w-full sm:w-[55%] border-2 border-gray-300 rounded-md"
           type="text"
+          onChange={onInputChange}
         />
         <button className="w-full sm:w-[15%] ml-2 bg-blue-500 text-white font-bold rounded-md">
           Detect
