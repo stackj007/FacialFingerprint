cd7be4c3da6b7208845cf38c37051a84ea899223
Ahmed Gamal <agamal0077@gmail.com>
2023-10-05 09:45:13 +0300

    making imageLinkForm responsive

--- src/Components/ImageLinkForm/ImageLinkForm.jsx
+++ src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -2,18 +2,18 @@ import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
   return (
-    <div className="p-5 w-[700px] mx-auto">
+    <div className="p-5 w-full mx-auto flex flex-col items-center">
       <p className="text-center font-bold text-lg m-5">
         This will detect faces in your pictures, give it a
         try
       </p>
 
-      <div className="Form flex justify-center shadow-md p-4 rounded-md">
+      <div className="Form flex justify-center shadow-md p-4 rounded-md w-full max-w-[700px]">
         <input
-          className="w-[55%]  border-2 border-gray-300 rounded-md"
+          className="w-full sm:w-[55%] border-2 border-gray-300 rounded-md"
           type="text"
         />
-        <button className="w-[15%] ml-2  bg-blue-500 text-white font-bold rounded-md">
+        <button className="w-full sm:w-[15%] ml-2 bg-blue-500 text-white font-bold rounded-md">
           Detect
         </button>
       </div>
